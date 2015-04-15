649ddcde8def82438b4037dce0f758f8d3621762
Tuss4 <tuss4dbfn@gmail.com>
2015-04-15 12:48:09 -0500

    Boom. That was easy.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":5050", "http server port")
+var file_location = "/home/tj/Documents/GO_DIR/src/github.com/tuss4/go_servefile/index.html"
+
+func serveIndex(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		http.ServeFile(w, req, file_location)
+	}
+}
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", serveIndex)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
